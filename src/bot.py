27fc30a6073a8da758edd022c8fb71a2d6049bf2
Emanuele Mazzotta <mazzotta.emanuele@gmail.com>
2018-11-03 16:21:12 +0100

    Add todo

--- src/bot.py
+++ src/bot.py
@@ -14,15 +14,29 @@ logger = logging.getLogger(__name__)
 token = open(join(dirname(realpath(__file__)), 'secret.key'), 'r+').read()
 bot = telebot.TeleBot(token)
 
-VALID_COMMANDS = ['say']
+VALID_COMMANDS = ['help', 'say']
+
+
+@bot.message_handler(commands=['help'])
+def help_info(message):
+    chat_id = message.chat.id
+    logger.info(f'Sending help to {chat_id}')
+    bot.send_message(chat_id, '/help - Show this text' +
+                     '\n/say - Ask the bot to say something in a bot voice')
 
 
 @bot.message_handler(commands=['say'])
 def say(message):
     chat_id = message.chat.id
-    voter = message.from_user.username
-    votee = message.text.split(' ')
-    bot.send_message(chat_id, 'hello! you asked me to say!')
+    say = message.text.replace('/say', '').strip()
+    if len(say) == 0:
+        bot.send_message(chat_id, f'I need you to tell me what to say. E.g. /say hello')
+    else:
+        bot.send_message(chat_id, f'hello! you asked me to say: {say}')
+        # TODO:
+        # apt-get update && apt-get install -y espeak ffmpeg
+        # echo "hello" | espeak -s 120 -ven-us+m1 --stdout | \
+        # ffmpeg -i - -ar 44100 -ac 2 -ab 192k -f mp3 final.mp3 &> /dev/null
 
 
 @bot.message_handler(func=lambda message: is_invalid_command(message))
@@ -33,7 +47,7 @@ def unknown_command(message):
 
 
 def is_invalid_command(message):
-    return message.document.mime_type == 'text/plain' and \
+    return not message.text or \
            all(message.text.startswith('/') and not message.text.startswith(f'/{valid}') for valid in VALID_COMMANDS)
 
 
